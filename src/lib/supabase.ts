955ea41d1fbea71ab5362ef591a4be72356449d1
Damilola Aguda <agudadamilola28@gmail.com>
2025-10-13 09:27:12 +0100

    supabase schema updates

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -50,7 +50,12 @@ export interface UniqueVisitor {
   brand_name?: string | null;
   verification_status?: 'pending' | 'verified' | 'unverified'| null;
   verification_id?: string;
-
+  hostel_id?: string
+  schools?: School
+  hostels?: Hostel
+  room?: string
+  is_hostel_merchant?: boolean
+  profile_picture?: string
 }
 
 export interface Product {
@@ -72,7 +77,26 @@ export interface Product {
     created_at?: string;
     merchant_id?: string;
     actual_merchant_id?: string;
-    brand_name?: string;
+    unique_visitors?: UniqueVisitor;
+    is_hostel_product: boolean; 
+    hostel_name?: string;
+    room?: string;
+    is_hostel_merchant?: boolean;
+    brand_name?: string | null;
+    product_category?: string;
+    search_words?: string[];
+
+}
+
+export interface HostelsProductUpdates {
+  id: string;
+  post_description: string;
+  post_images: string[];
+  created_at: string;
+  merchant_id: string;
+  unique_visitors?: UniqueVisitor;
+  post_category: string;
+  search_words: string[];
 }
 
 export interface RequestLog {
@@ -95,6 +119,13 @@ export interface School {
   created_at: string;
 }
 
+export interface Hostel {
+  id: string
+  name: string
+  school_id: string
+  schools?: School
+}
+
 
 export interface MerchantAnalytics {
   id: string;
@@ -105,6 +136,15 @@ export interface MerchantAnalytics {
   user_id: string;
   created_at?: string;
 }
+
+export interface ProductCategory {
+  id: string;
+  category_name: string;
+  category_image: string;
+  category_list: string[];
+  created_at: string;
+}
+
 
 // Add function to delete a request log
 export async function deleteRequestLog(requestId: string): Promise<{ success: boolean; error?: string }> {
